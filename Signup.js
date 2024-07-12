f690b777d90ff8a46abab1038b41533332339290
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-07-12 10:44:53 -0400

    Initial validation setuo

--- Signup.js
+++ Signup.js
@@ -5,6 +5,11 @@ setTimeout(() => {
   document.querySelector(".Signup").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("submitted Signup");
+    if (signup.elements["email"].value === "") {
+      alert("Please enter an email");
+    } else if (signup.elements["password"].value === "") {
+      alert("Please enter a password");
+    }
   });
 
   document.querySelector(".Login").addEventListener("submit", (e) => {
