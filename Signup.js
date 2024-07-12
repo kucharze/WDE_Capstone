f440a5c132c1139de8a279c4defdf457036bd127
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-07-11 21:56:29 -0400

    Added event listeners

--- Signup.js
+++ Signup.js
@@ -1,2 +1,10 @@
 let signup = document.querySelector(".Signup");
 let login = document.querySelector("Login");
+
+login.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+signup.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
