b206f8ee10020a4c4f03e02739dac983c30056c4
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-07-12 13:15:52 -0400

    Initial logic for packshop page

---
+++ shop.js
@@ -0,0 +1,20 @@
+let priceList = {
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+  ["Unfinity"]: 6,
+};
+
+let total = 0;
+
+setTimeout(() => {
+  let shop = document.querySelector(".shop");
+  let cart = document.querySelector(".shoppingCart");
+  let checkout = document.querySelector(".checkout");
+  let total = document.querySelector(".total");
+}, 500);
